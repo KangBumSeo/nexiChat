6b424c0c978d5d7e1ce3da8447927f46af6205db
S-jeongheun <sjhtest00@gmail.com>
2022-08-23 11:22:57 +0900

    20220823 송정흔 test

---
+++ Nexicure Web Application/src/main/java/com/nexicure/nim/services/interf/testServiceInterface.java
@@ -0,0 +1,8 @@
+package com.nexicure.nim.services.interf;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public interface testServiceInterface {
+	public ArrayList<HashMap<String, Object>> testChat();
+}
