0d949f53c32b19a440cd5516d2aac8e5654fece3
KangBumSeo <soe09@naver.com>
2022-08-23 11:34:17 +0900

    remove

--- Nexicure Web Application/src/main/java/com/nexicure/nim/services/interf/testServiceInterface.java
+++
@@ -1,8 +0,0 @@
-package com.nexicure.nim.services.interf;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public interface testServiceInterface {
-	public ArrayList<HashMap<String, Object>> testChat();
-}
